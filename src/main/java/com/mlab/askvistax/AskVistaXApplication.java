0168fb5f0f1b75e89284cb7884d8c602ca653a01
yizhilsy <1937714332@qq.com>
2025-06-14 12:42:21 +0800

    AskVistaX Projection Init

---
+++ src/main/java/com/mlab/askvistax/AskVistaXApplication.java
@@ -0,0 +1,13 @@
+package com.mlab.askvistax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskVistaXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AskVistaXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mlab/askvistax/AskVistaXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlab.askvistax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskVistaXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
